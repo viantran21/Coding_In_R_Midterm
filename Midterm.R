d994dda694edd1e9e6b58d5d5248610b52fed51b
Vian Tran <you@example.com>
2024-07-31 18:36:03 -0400

    cleanned the data

--- Midterm.R
+++ Midterm.R
@@ -3,6 +3,7 @@
 #Vian Tran 
 #Summer 2024
 
+rm(list = ls())
 #install packages necessary for Exploratory Data Analysis
 #install.packages("tidyverse")
 #install.packages("funModeling")
@@ -20,7 +21,6 @@ trip <- read_csv("trip.csv")
 weather <- read_csv("weather.csv")
 
 #get an understanding of get dataset to help clean the data
-
 summary(station)
 str(station)
 dim(station)
@@ -36,6 +36,40 @@ str(weather)
 dim(weather)
 names(weather)
 
+#CLEAN
+#remove duplicates 
+station_clean <- distinct(station)
+trip_clean <- distinct(trip)
+weather_clean <- distinct(weather)
 
+#station - the number of dock_count ranged from 11-27, change it to a factor to see the groups of dock_count
+station_clean$dock_count <- as.factor(station_clean$dock_count)
+summary(station_clean)
 
+#use lubidate to change the dates to Date class
+library(lubridate)
+#station - installation_date change to MDY
+station_clean$installation_date <- mdy(station_clean$installation_date)
+class(station_clean$installation_date)
+#trip - start_date change to MDY_HM
+trip_clean$start_date <- mdy_hm(trip_clean$start_date)
+class(trip_clean$start_date)
+#trip - end_date change to MDY_HM
+trip_clean$end_date <- mdy_hm(trip_clean$end_date)
+class(trip_clean$end_date)
+#weather - date change to MDY
+weather_clean$date <- mdy(weather_clean$date)
+class(weather_clean$date)
+
+#trip - change zip codes to only include USA zip codes (based on the areas the hubs are posted in), change the rest to NA 
+trip_clean <- trip_clean %>%
+  mutate(zip_code = ifelse(grepl("^[0-9]{5}$", zip_code) & !zip_code %in% c("99999", "0"), zip_code, NA))
+#keep the ZIP code if it starts with a digit (0-9) and it is exactly 5 digits long, otherwise set it to NA
+#also ensure "99999" and "0" are set to NA
+
+#weather - take the T's in precipitate and make them NAs
+weather_clean <- weather_clean %>% 
+  mutate(precipitation_inches = ifelse(precipitation_inches == "T", NA, precipitation_inches))
+#precipitation_inches is currently a character, change to numeric
+weather_clean$precipitation_inches <- as.numeric(weather_clean$precipitation_inches)
 
